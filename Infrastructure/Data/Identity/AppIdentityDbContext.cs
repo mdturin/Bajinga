9a2837e3738df7124cee1f4f10e52a235e98970a
Md Turin <matkhan01@yahoo.com>
2023-07-01 00:44:25 +0600

    identity and seed-data added

---
+++ Infrastructure/Data/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Identity;
+
+public class AppIdentityDbContext : IdentityDbContext<AppUser>
+{
+    public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+        : base(options) { }
+}
